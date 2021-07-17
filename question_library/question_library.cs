09365191f2afb43f02cbd406baad92d781e19e9b
c2sq <cicy.zhou7@gmail.com>
2021-07-17 22:12:43 +1000

    initial commit

---
+++ question_library/question_library.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace question_library {
+    public class question_library {
+        public string Python { get; set; }
+        public string Java { get; set; }
+
+        public string C { get; set; }
+        
+        public string PHP { get; set; }
+        
+        public string Cpp { get; set; }
+
+        public string Javascript { get; set; }
+        
+        public string Cs { get; set; }
+        
+        public string Ruby { get; set; }
+        
+        public string ObjC { get; set; }
+        
+        
+        
+        
+        
+
+    }
+}
---
+++ questions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to learn programming because it...");
+            Console.WriteLine("1. is a fun thing to learn.");
+            Console.WriteLine("2. is useful for me.");
+            string inp_choice = Console.ReadLine("Input 1 or 2: ");
+            int choice = Int32.Parse(inp_choice);
+
+            if (choice == 1) 
+            {
+                Console.WriteLine("Have a particular area of interest in mind?");
+                inp_choice = Console.ReadLine("Y or N: ");
+                if (inp_choice == "Y" || inp_choice == "y") {
+                    interested_area();
+
+                }
+            }
+
+            else if (choice == 2) 
+            {
+
+            }
+
+            else 
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+        }
+
+        public static int interest_area() 
+        {
+
+        }
+    
+
+    }
+}
